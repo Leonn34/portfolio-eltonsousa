4912c6e5569d102ad8097740eeadbd99e2783fbc
Leonn34 <eltonsousadesigner@gmail.com>
2020-08-14 23:28:06 -0400

    add

---
+++ public/_js/script.js
@@ -0,0 +1,11 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "slide", // nome do atributo
+    nav: "true", // menu ativo
+    time: 5000, // tempo transição
+  });
+}
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
